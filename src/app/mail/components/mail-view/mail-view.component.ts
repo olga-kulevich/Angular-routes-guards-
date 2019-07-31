907a5094b04a66f543d695b6bd202eb992dcb1f0
Volha Kulevich <volha.kulevich@exposit-ds.com>
2019-07-31 19:25:25 +0300

    lesson 63 Guards with CanDeactivate

--- src/app/mail/components/mail-view/mail-view.component.ts
+++ src/app/mail/components/mail-view/mail-view.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {Observable} from "rxjs";
 import {Mail} from "../../models/mail.interface";
@@ -12,9 +12,35 @@ import {pluck} from "rxjs/operators";
       <h2>{{ (message | async).from }}</h2>
       <h2>{{ (message | async).full }}</h2>
     </div>
+    <div class="mail-reply">
+      <textarea
+        (change)="updateReply($event.target.value)"
+          placeholder="Type your reply..."
+      value="reply">
+      </textarea>
+      <button type="button" (click)="sendReply()">
+        Send
+      </button>
+    </div>
   `
 })
-export class MailViewComponent {
+export class MailViewComponent implements OnInit{
+  reply='';
+
   message: Observable<Mail> = this.route.data.pipe(pluck('message'));
   constructor(private route: ActivatedRoute){}
+
+  ngOnInit() {
+    this.route.params.subscribe(() => {
+      this.reply = '';
+    })
+  }
+
+  updateReply(value: string) {
+    this.reply = value;
+  }
+
+  sendReply() {
+   console.log('sent!', this.reply);
+  }
 }
